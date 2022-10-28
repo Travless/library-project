684fc0e930df70951fe265e38b44f78235479b2c
Travless <travis.white0101@gmail.com>
2022-10-28 15:23:37 -0400

    impliment individual, unique cards ffor each book entry via JS

--- app.js
+++ app.js
@@ -12,34 +12,78 @@ function addBookToLibrary(book) {
     return myLibrary;
 }
 
-let book1 = new Book('Stephen King', 'It', 1000, 'Yes');
-
-addBookToLibrary(book1);
+const form = document.getElementById('form');
 
 const bookCardsContainer = document.querySelector('.book-cards-container');
 
-const addBookBtn = document.getElementById('add-book-btn');
-
-for (let i = 0; i < myLibrary.length; i++) {
+function cardGen(){
     const bookCard = document.createElement('div');
-    bookCard.classList.add('book-card');
+        bookCard.classList.add('book-card');
+    
+        const bookCardAuthor = document.createElement('div');
+        bookCardAuthor.textContent = `Author: ${newAuthor.value}`;
+        bookCard.append(bookCardAuthor);
+    
+        const bookCardTitle = document.createElement('div');
+        bookCardTitle.textContent = `Title: ${newTitle.value}`;
+        bookCard.append(bookCardTitle);
+    
+        const bookCardPages = document.createElement('div');
+        bookCardPages.textContent = `Number of Pages: ${newPages.value}`;
+        bookCard.append(bookCardPages);
+    
+        const bookCardRead = document.createElement('div');
+        bookCardRead.textContent = `Read: ${newRead.value}`;
+        bookCard.append(bookCardRead);
 
-    const bookCardAuthor = document.createElement('div');
-    bookCardAuthor.textContent = `Author: ${myLibrary[i].author}`;
-    bookCard.append(bookCardAuthor);
-
-    const bookCardTitle = document.createElement('div');
-    bookCardTitle.textContent = `Title: ${myLibrary[i].title}`;
-    bookCard.append(bookCardTitle);
-
-    const bookCardPages = document.createElement('div');
-    bookCardPages.textContent = `Number of Pages: ${myLibrary[i].pages}`;
-    bookCard.append(bookCardPages);
-
-    const bookCardRead = document.createElement('div');
-    bookCardRead.textContent = `Read: ${myLibrary[i].read}`;
-    bookCard.append(bookCardRead);
-
-    bookCardsContainer.append(bookCard);
+        const bookCardDel = document.createElement('button');
+        bookCardDel.classList.add('book-del-btn');
+        bookCardDel.textContent = 'Delete';
+        bookCard.append(bookCardDel);
+    
+        bookCardsContainer.append(bookCard);
 }
 
+// function cardGen(){
+//     for (let i = 0; i < myLibrary.length; i++) {
+//         const bookCard = document.createElement('div');
+//         bookCard.classList.add('book-card');
+    
+//         const bookCardAuthor = document.createElement('div');
+//         bookCardAuthor.textContent = `Author: ${myLibrary[i].author}`;
+//         bookCard.append(bookCardAuthor);
+    
+//         const bookCardTitle = document.createElement('div');
+//         bookCardTitle.textContent = `Title: ${myLibrary[i].title}`;
+//         bookCard.append(bookCardTitle);
+    
+//         const bookCardPages = document.createElement('div');
+//         bookCardPages.textContent = `Number of Pages: ${myLibrary[i].pages}`;
+//         bookCard.append(bookCardPages);
+    
+//         const bookCardRead = document.createElement('div');
+//         bookCardRead.textContent = `Read: ${myLibrary[i].read}`;
+//         bookCard.append(bookCardRead);
+    
+//         bookCardsContainer.append(bookCard);
+//     }
+// };
+
+form.addEventListener('submit', function(event){
+    event.preventDefault()
+
+    const newAuthor = document.getElementById('newAuthor').value;
+    const newTitle = document.getElementById('newTitle').value;
+    const newPages = document.getElementById('newPages').value;
+    const newRead = document.getElementById('newRead').value;
+
+    const newBook = new Book(newAuthor, newTitle, newPages, newRead);
+
+    addBookToLibrary(newBook);
+
+    cardGen();
+})
+
+bookCardDel.addEventListener("click", function(event){
+    console.log(myLibrary.prototype.findIndex());
+})
